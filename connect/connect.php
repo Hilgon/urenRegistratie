c220c84c9dd01777c44c242bd5cf90f7bf0d8574
Terry Zhou <terry-007@live.nl>
2017-01-24 10:16:24 +0100

    Delete connect.php

--- connect/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=stagedagen',"root","");
-$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch(PDOexception $e) {
-    echo 'error: ' .$e->getMessage();
-}
-?>
